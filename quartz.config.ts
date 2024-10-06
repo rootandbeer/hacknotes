30603651e985e525baa2b27046450c4cb5a057fe
rootandbeer <139183768+rootandbeer@users.noreply.github.com>
2024-10-05 17:55:11 -0700

    Quartz sync: Oct 5, 2024, 5:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Templates"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
